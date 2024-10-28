76ab1fe5c81a48f22402628d4e3d3186953862e0
ICKostiantyn.Ivanov <ICKostiantyn.Ivanov@amwell.com>
2024-10-28 18:24:38 +0100

    add async configuration

---
+++ src/main/java/com/example/demo/config/SpringAsyncConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10000);
+        executor.setKeepAliveSeconds(60);
+
+        return executor;
+    }
+}
